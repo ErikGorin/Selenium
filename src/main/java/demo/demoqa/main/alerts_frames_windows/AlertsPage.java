847859c3c6a8a781d980f5fe3e5af14a7e1bc411
Ion <ion@debatezone.eu>
2025-01-06 11:20:24 +0200

    Init commit alert prompt

--- src/main/java/demo/demoqa/main/alerts_frames_windows/AlertsPage.java
+++ src/main/java/demo/demoqa/main/alerts_frames_windows/AlertsPage.java
@@ -1,15 +1,35 @@
 package demo.demoqa.main.alerts_frames_windows;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class AlertsPage extends Alert_Frames_WindowsPage {
 
     private By informationAlertButton = By.id("alertButton");
     private By confirmationAlertButton = By.id("confirmButton");
     private By confirmationResult = By.id("confirmResult");
+    private By promptAlertButton = By.id("promtButton");
+    private By promptResult = By.id("promptResult");
+
 
     public void clickInformationAlertButton() {
-        click(informationAlertButton);
+        try {
+            click(informationAlertButton);
+        } catch (ElementClickInterceptedException e) {
+            System.out.println("Element click intercepted. Handling the obstruction.");
+            List<WebElement> iframes = driver.findElements(By.cssSelector("iframe"));
+            for (WebElement iframe : iframes) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].remove();", iframe);
+            }
+            click(informationAlertButton);
+        } catch (Exception e) {
+            System.out.println("Unexpected error while clicking information alert button: " + e.getMessage());
+            throw e;
+        }
     }
 
     public void clickConfirmationAlertButton() {
@@ -18,5 +38,26 @@ public class AlertsPage extends Alert_Frames_WindowsPage {
 
     public String getConfirmationResult() {
         return find(confirmationResult).getText();
+    }
+
+
+    public void clickPromptAlertButton() {
+        try {
+            click(promptAlertButton);
+        } catch (ElementClickInterceptedException e) {
+            System.out.println("Element click intercepted. Handling the obstruction.");
+            List<WebElement> iframes = driver.findElements(By.cssSelector("iframe"));
+            for (WebElement iframe : iframes) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].remove();", iframe);
+            }
+            click(promptAlertButton);
+        } catch (Exception e) {
+            System.out.println("Unexpected error while clicking information alert button: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    public String getPromptAlertResult() {
+        return find(promptResult).getText();
     }
 }
--- src/test/java/part_3_4/demoqa/com/test/part_4/alerts/AlertsTest.java
+++ src/test/java/part_3_4/demoqa/com/test/part_4/alerts/AlertsTest.java
@@ -24,6 +24,18 @@ public class AlertsTest extends BaseTest {
         String actualConfirmationResult = alertsPage.getConfirmationResult();
         String expectedConfirmationResult = "You selected Cancel";
         Assert.assertEquals(actualConfirmationResult, expectedConfirmationResult, "\n  You did not select Cancel \n");
-
     }
+
+
+    public void testPromptAlert() {
+        String alertText = "Erik Gorin learn Java";
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickPromptAlertButton();
+        setAlertText(alertText);
+        acceptAlert();
+        String actualResult = alertsPage.getPromptAlertResult();
+        String expectedResult = "You entered " + alertText;
+        Assert.assertEquals(actualResult, expectedResult, "\n Expected and Actual Result Do Not Match\n");
+    }
+
 }
